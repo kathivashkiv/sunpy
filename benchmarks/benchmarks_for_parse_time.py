35bac176c68254cfc7c13c34b81500c071c70353
kathivashkiv <kate777iv@gmail.com>
2020-12-10 14:04:36 +0200

    Minor push to activate actions on asv-start

--- benchmarks/benchmarks_for_parse_time.py
+++ benchmarks/benchmarks_for_parse_time.py
@@ -10,6 +10,7 @@ class TimeSuite:
     """
     def time_parse_time(self):
        t = parse_time('1995-12-31 23:59:60')
+       t1 = t5 = parse_time((2001, 1, 1, 12, 12, 12, 8899))
 
 class MemSuite:
     def mem_parse_time(self):
